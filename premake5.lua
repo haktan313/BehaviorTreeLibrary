ec4fc841c2aa5a6e886b54767399933951297faf
haktan0313 <134720352+haktan313@users.noreply.github.com>
2025-11-23 00:11:35 +0100

    libs

--- premake5.lua
+++ premake5.lua
@@ -16,14 +16,30 @@ project "BehaviorTreeProject"
     files {
         "src/**.h",
         "src/**.hpp",
-        "src/**.cpp"
+        "src/**.cpp",
+        "libs/imgui-1.92.4/*.cpp",
+        "libs/imgui-1.92.4/backends/imgui_impl_glfw.cpp",
+        "libs/imgui-1.92.4/backends/imgui_impl_opengl3.cpp"
     }
  
     includedirs {
-           "src",
-           "src/BehaviorTreeThings",
-           "src/BehaviorTreeThings/Core",
-           "src/BehaviorTreeThings/CustomThings"
+        "src",
+        "src/BehaviorTreeThings",
+        "src/BehaviorTreeThings/Core",
+        "src/BehaviorTreeThings/CustomThings",
+        "libs/glad/include",
+        "libs/glfw-3.4.bin.WIN64/include",
+        "libs/imgui-1.92.4",
+        "libs/imgui-1.92.4/backends"
+    }
+
+    libdirs {
+        "libs/glfw-3.4.bin.WIN64/lib-vc2022"
+    }
+
+    links {
+        "glfw3",
+        "opengl32"
     }
 
     filter "system:windows"
